63768cb8fe6cf6ff50b378476e7dcdd03bef0af8
Claire <102723844+tevescl@users.noreply.github.com>
2024-07-17 15:59:27 +1200

    test file

--- Scanner/program1.py
+++ Scanner/program1.py
@@ -2,3 +2,19 @@ z := 2;
 if z < 3 then
   z := 1
 end
+
+# Running the scanner/ply-scanner by python3 scanner.py < program1.py > tokens will produce the file tokens which contains
+
+# ID z
+# BEC
+# NUM 2
+# SEM
+# IF
+# ID z
+# LESS
+# NUM 3
+# THEN
+# ID z
+# BEC
+# NUM 1
+# END
