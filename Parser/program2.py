e727e5ba7ea61cebd2564f75947d5a6b50e1ea9f
Claire <102723844+tevescl@users.noreply.github.com>
2024-07-17 16:16:28 +1200

    test file

--- Parser/program2.py
+++ Parser/program2.py
@@ -4,3 +4,22 @@ if z < 3 then
 else
   z := 0
 end
+
+# Running the parser/ply-parser by python3 parser.py < program2.py > tree will produce the file tree which contains
+
+# Statements
+#     Assign
+#         z
+#         2
+#     If-Else
+#         <
+#             z
+#             3
+#         Statements
+#             Assign
+#                 z
+#                 1
+#         Statements
+#             Assign
+#                 z
+#                 0
